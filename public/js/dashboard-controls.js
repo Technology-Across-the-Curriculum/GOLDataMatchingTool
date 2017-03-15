5f93e5d9d0bd43f9afc761a105447f3046a03fae
Nathan Healea <nate.healea@gmail.com>
2017-03-15 13:23:32 -0700

    Added table swtiching

--- public/js/dashboard-controls.js
+++ public/js/dashboard-controls.js
@@ -3,32 +3,59 @@
  */
 var studentModal = null;
 var btnDetail = 'btn-detail-';
+var dataTable = null;
 
-$(function(){
+$(function () {
 
     // Load modal and save the object
     studentModal = $('#student-modal').modal({'show': false});
 
 
-    $("[id*='" + btnDetail + "']").on('click',function(){
+    /*$("[id*='" + btnDetail + "']").on('click',function(){
+
+     var id = this.id.replace(btnDetail, '');
+
+     // Show the modal
+     studentModal.modal('show');
+     $.when(getStudentData(id)).done(function(json){
+
+     var data = JSON.parse(json);
+     console.log(data);
+     buildTable(data);
+
+
+     })
+
+     })*/
+
+    // Modal Close Button
+    $('#btn-modal-close').on('click', function () {
+        emptyTable();
+    });
+
+    // Modal Exit Button
+    $('#btn-modal-exit').on('click', function () {
+        emptyTable();
+    });
+
+    // Onclick events for table Details button
+    $('#classlist-table tbody').on('click', 'div.btn', function () {
 
         var id = this.id.replace(btnDetail, '');
-
-        // Show the modal
         studentModal.modal('show');
-        $.when(getStudentData(id)).done(function(json){
+        $.when(getStudentData(id)).done(function (json) {
 
             var data = JSON.parse(json);
             console.log(data);
+
             buildTable(data);
 
 
         })
-
     })
 });
 
-function getStudentData(id){
+function getStudentData(id) {
     return $.ajax({
         type: "get",
         async: false,
@@ -36,17 +63,25 @@ function getStudentData(id){
     })
 }
 
-function buildTable(data){
-    $('#student-data').DataTable( {
-        "data":data,
+function buildTable(data) {
+    dataTable = $('#student-data').DataTable({
+        "data": data,
+        "destroy": true,
         "columns": [
-            { "data": "id" },
-            { "data": "first" },
-            { "data": "last" },
-            { "data": "lmsid" },
-            { "data": "email" }
+            {"data": "id"},
+            {"data": "first"},
+            {"data": "last"},
+            {"data": "lmsid"},
+            {"data": "email"}
         ]
-    } );
+    });
+
+    
+
+}
+
+function emptyTable() {
+    dataTable.destroy();
 }
 
 
